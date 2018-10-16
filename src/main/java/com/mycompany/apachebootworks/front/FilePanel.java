a0f80ad699890932a9c7263b35a37817b5be0067
tommib <tommib@03TBj-1117.masino.local>
2018-10-16 10:56:29 +0300

    More updates. Ajaxlinks now working

---
+++ src/main/java/com/mycompany/apachebootworks/front/FilePanel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.apachebootworks.front;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ *
+ * @author tommib
+ */
+public class FilePanel extends Panel {
+
+    public FilePanel(String id) {
+        super(id);
+    }
+}
--- src/main/java/com/mycompany/apachebootworks/front/Index.java
+++ src/main/java/com/mycompany/apachebootworks/front/Index.java
@@ -19,20 +19,59 @@ import org.apache.wicket.markup.html.WebPage;
 @WicketHomePage
 public class Index extends WebPage {
 
-    private Component homePanel = new HomePanel("home");
-    private Component aboutPanel = new AboutPanel("about");
+    private Component homePanel = new HomePanel("home"); // "home" string within HomePanel constructor is HTML TAG 
+    private Component aboutPanel = new AboutPanel("about"); // "about" string within AboutPAnel constructor is HTML TAG 
+    private Component filePanel = new FilePanel("files"); // "files" string within FilePanel constructor is HTML TAG 
 
     public Index() {
-        add(homePanel);
-        add(aboutPanel);
-        homePanel.setOutputMarkupId(true);
-        aboutPanel.setOutputMarkupId(true);
-        add(new AjaxLink<Void>("click") {
+
+        add(homePanel); // Add homePanel to be used in HTML (via constructors id)
+        add(aboutPanel); // Add aboutPanel to be used in HTML (via constructors id)
+        aboutPanel.setVisible(false); // hide aboutPanel
+        add(filePanel); // add filePanel to be used in HTML (via constructors id)
+        filePanel.setVisible(false); // hide filePanel
+        
+
+        homePanel.setOutputMarkupPlaceholderTag(true); // make homePanel modifiable via AJAX
+        aboutPanel.setOutputMarkupPlaceholderTag(true); // make aboutPanel modifiable via AJAX
+        filePanel.setOutputMarkupPlaceholderTag(true); // make filePanel modifiable via AJAX
+
+        add(new AjaxLink<Void>("click") { // AJAXLink and its HTML id.
             @Override
-            public void onClick(AjaxRequestTarget art) {
-                homePanel.setVisible(false);
-                aboutPanel.setVisible(true);
+            public void onClick(AjaxRequestTarget art) { // upon clicking HTML id, do this.
+
+                homePanel.setVisible(true);
+                aboutPanel.setVisible(false);
+                filePanel.setVisible(false);
+                art.add(homePanel);
+                art.add(aboutPanel);
+                art.add(filePanel);
             }
         });
+        add(new AjaxLink<Void>("click2") {
+            @Override
+            public void onClick(AjaxRequestTarget art) {
+
+                homePanel.setVisible(false);
+                aboutPanel.setVisible(true);
+                filePanel.setVisible(false);
+                art.add(homePanel);
+                art.add(aboutPanel);
+                art.add(filePanel);
+            }
+        });
+        add(new AjaxLink<Void>("click3") {
+            @Override
+            public void onClick(AjaxRequestTarget art) {
+
+                homePanel.setVisible(false);
+                aboutPanel.setVisible(false);
+                filePanel.setVisible(true);
+                art.add(homePanel);
+                art.add(aboutPanel);
+                art.add(filePanel);
+            }
+        });
+
     }
 }
